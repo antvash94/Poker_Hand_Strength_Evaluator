2f6206cdf2c99d053ac19ddb4ee958298d19834a
anton <ant.vash94@gmail.com>
2020-12-19 22:45:35 +0300

    poker

--- algorithm.py
+++ algorithm.py
@@ -29,6 +29,89 @@ def counter(board='', hand=''):
     return result
 
 
+def separator(hand, value):
+    if value == 2:
+        counted_card = counter(hand)
+        card = get_key(2, counted_card[0])
+        hand_list = [hand[x: x+2] for x in range(0, len(hand), 2)]
+        pair_one = []
+        cards = []
+        for i in hand_list:
+            if i[0] == card:
+                pair_one.append(i)
+            else:
+                cards.append(i)
+        separate_card = (''.join(pair_one), ''.join(cards))
+        return separate_card
+
+    elif value == 3:
+        counted_card = counter(hand)
+        card_one = get_key(2, counted_card[0])
+        card_two = get_key(1, counted_card[0])
+        hand_list = [hand[x: x+2] for x in range(0, len(hand), 2)]
+        pair_one = []
+        pair_two = []
+        card = []
+        for i in hand_list:
+            if i[0] == card_one:
+                pair_one.append(i)
+            elif i[0] == card_two:
+                card.append(i)
+            else:
+                pair_two.append(i)
+        separate_card = (''.join(pair_one), ''.join(pair_two), card[0])
+        return separate_card
+
+    elif value == 4:
+        counted_card = counter(hand)
+        card = get_key(3, counted_card[0])
+        hand_list = [hand[x: x + 2] for x in range(0, len(hand), 2)]
+        three = []
+        cards = []
+        for i in hand_list:
+            if i[0] == card:
+                three.append(i)
+            else:
+                cards.append(i)
+        separate_card = (''.join(three), ''.join(cards))
+        return separate_card
+
+    elif value == 8:
+        counted_card = counter(hand)
+        card = get_key(4, counted_card[0])
+        hand_list = [hand[x: x + 2] for x in range(0, len(hand), 2)]
+        four = []
+        cards = []
+        for i in hand_list:
+            if i[0] == card:
+                four.append(i)
+            else:
+                cards.append(i)
+        separate_card = (''.join(four), cards[0])
+        return separate_card
+
+    elif value == 7:
+        counted_card = counter(hand)
+        card_one = get_key(2, counted_card[0])
+        card_two = get_key(3, counted_card[0])
+        hand_list = [hand[x: x + 2] for x in range(0, len(hand), 2)]
+        three = []
+        two = []
+        for i in hand_list:
+            if i[0] == card_one:
+                two.append(i)
+            elif i[0] == card_two:
+                three.append(i)
+        separate_card = (''.join(three), ''.join(two))
+        return separate_card
+
+
+def get_key(val, my_dict):
+    for key, value in my_dict.items():
+        if val == value:
+            return key
+
+
 def check_straight(board='', hand=''):
     cards = sorted_cards(board, hand)
     card_list = ''.join([i[0] for i in cards])
@@ -121,140 +204,184 @@ def check_hand(board='', hand=''):
     result = (max(card_rating), hand)
     return result
 
-
-solution_list = [(2, '5s5d7s4dQd'), (2, '3cKs4cKdJs'), (3, '4s4hTsTh9h'),
-                 (5, '7h6h8d9cTc'), (6, '2hAhKh4hKh'), (7, 'QcQd7c7c7d'), (9, '6c2c3c4c5c')]
-
-
-
 def solution_sort(hands):
-    # Устанавливаем swapped в True, чтобы цикл запустился хотя бы один раз
     swapped = True
     while swapped:
         swapped = False
         for i in range(len(hands) - 1):
             if hands[i][0] > hands[i + 1][0]:
-                # Меняем элементы
                 hands[i], hands[i + 1] = hands[i + 1], hands[i]
-                # Устанавливаем swapped в True для следующей итерации
                 swapped = True
             elif hands[i][0] == hands[i + 1][0]:
-                if equal_sort(hands[i],hands[i + 1], hands[i][0]):
+                if equal_sort(hands[i][1], hands[i + 1][1], hands[i][0]):
                     hands[i], hands[i + 1] = hands[i + 1], hands[i]
                     swapped = True
 
 
-
-# def equal_sort(first_hand, other_hand, hand_rating):
-#     if hand_rating == 1:
-#         if high_card_equal(first_hand, other_hand):
-
-
-
-#     elif hand_rating == 2:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 3:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 4:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 5:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 6:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 7:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 8:
-#         if True:
-#             return True
-#         else:
-#             return False
-#     elif hand_rating == 9:
-#         if True:
-#             return True
-#         else:
-#             return False
-
-hand1 ='QsJd7s4dQd'
-hand2 = '3cQs4cQdTs'
-
-def get_key(val, my_dict):
-    for key, value in my_dict.items():
-         if val == value:
-             return key
-
-def separator(hand, value):
-    if value == 2:
-        counted_card = counter(hand)
-        card = get_key(2, counted_card[0])
-        pair = []
-        cards = []
-        for i in hand:
-            if i[0] == card:
-                pair.append(i)
-            else:
-                cards.append(i)
-        separate_card = (pair,cards)
-        return separate_card
+def equal_sort(first_hand, other_hand, hand_rating):
+    if hand_rating == 1 and high_card_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 2 and pair_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 3 and two_pairs_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 4 and three_of_a_kind_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 5 and straight_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 6 and high_card_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 7 and full_house_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 8 and four_of_a_kind_equal(first_hand, other_hand):
+        return True
+    elif hand_rating == 9 and straight_equal(first_hand, other_hand):
+        return True
+    else:
+        return False
 
 
+def high_card(card1, card2):
+    if values[card1[0]] > values[card2[0]]:
+        return True
+    else:
+        return False
 
 
-def high_card_equal(hand1,hand2):
+def high_card_equal(hand1, hand2):
     hand1_list = [i[2] for i in sorted_cards(hand=hand1)]
     hand2_list = [i[2] for i in sorted_cards(hand=hand2)]
     n = -1
-    for i in range(len(hand1_list)-1):
-        if hand1_list[n] > hand2_list[n]:
-            print(hand1_list)
-            return True
-        elif hand1_list[n] == hand2_list[n]:
-            n -= 1
-        else:
-            print(hand2_list)
-            return False
+    if hand1_list == hand2_list:
+        flag = 'Equal'
+        return flag
+
+    else:
+        for i in range(len(hand1_list)-1):
+            if hand1_list[n] > hand2_list[n]:
+                return True
+            elif hand1_list[n] == hand2_list[n]:
+                n -= 1
+            else:
+                return False
 
 
 def pair_equal(hand1, hand2):
-    pass
+    hand_one_list = separator(hand1, 2)
+    hand_two_list = separator(hand2, 2)
+    if high_card_equal(hand_one_list[0], hand_two_list[0]) == 'Equal':
+        if high_card_equal(hand_one_list[1], hand_two_list[1]):
+            return True
+        else:
+            return False
+    elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+        return True
+
+    else:
+        return False
 
 
-
-def two_pair_equal(hand1, hand2):
-    pass
+def two_pairs_equal(hand1, hand2):
+    hand_one_list = separator(hand1, 3)
+    hand_two_list = separator(hand2, 3)
+    if high_card_equal(hand_one_list[0], hand_two_list[0]) == 'Equal':
+        print('sa')
+        if high_card_equal(hand_one_list[1], hand_two_list[1]) == 'Equal':
+            print('wqqe')
+            if high_card(hand_one_list[2], hand_two_list[2]):
+                print('adc')
+                return True
+            else:
+                return False
+        elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+            return True
+        else:
+            return False
+    elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+        return True
+    else:
+        return False
 
 
 def three_of_a_kind_equal(hand1, hand2):
-    pass
+    hand_one_list = separator(hand1, 4)
+    hand_two_list = separator(hand2, 4)
+    if high_card_equal(hand_one_list[0], hand_two_list[0]) == 'Equal':
+        if high_card_equal(hand_one_list[1], hand_two_list[1]):
+            return True
+        else:
+            return False
+    elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+        return True
+
+    else:
+        return False
 
 
-def four_of_a_kind(hand1, hand2):
-    pass
+def four_of_a_kind_equal(hand1, hand2):
+    hand_one_list = separator(hand1, 8)
+    hand_two_list = separator(hand2, 8)
+    if high_card_equal(hand_one_list[0], hand_two_list[0]) == 'Equal':
+        if high_card(hand_one_list[1], hand_two_list[1]):
+            return True
+        else:
+            return False
+    elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+        return True
+
+    else:
+        return False
 
 
-def full_house_equal(hand1,hand2):
-    pass
+def full_house_equal(hand1, hand2):
+    hand_one_list = separator(hand1, 7)
+    hand_two_list = separator(hand2, 7)
+    if high_card_equal(hand_one_list[0], hand_two_list[0]) == 'Equal':
+        if high_card_equal(hand_one_list[1], hand_two_list[1]):
+            return True
+        else:
+            return False
+    elif high_card_equal(hand_one_list[0], hand_two_list[0]):
+        return True
+
+    else:
+        return False
 
 
+def straight_equal(hand1, hand2):
+    hand1_list = ''.join([i[0] for i in sorted_cards(hand=hand1)])
+    print(hand1_list)
+    if hand1_list =='2345A':
+        return False
+    elif high_card_equal(hand1, hand2) and hand1_list!='2345A':
+        return True
+    else:
+        return False
 
-print(separator('7hQsQh8c9h', 2))
+
+def output(hands):
+    solution = []
+    for hand in hands:
+        solution.append(check_hand(hand=hand))
+    solution_sort(solution)
+    return ' '.join([x[1] for x in solution])
+
+
+with open('test.txt', 'r') as file:
+    for line in file.readlines():
+        card = line.rstrip().split(' ')
+        if card[0] == 'texas-holdem':
+            print('Not ready yet')
+        elif card[0] == 'omaha-holdem':
+            print('Not ready yet')
+        elif card[0] == 'five-card-draw':
+            hands = card[1:]
+            with open('solution.txt', 'a') as file:
+                file.writelines(output(hands)+'\n')
+
+        else:
+            print('Error')
+
 
 
 
